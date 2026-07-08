3dace38f59dc37ade27fb501279036b7c5d9aa64
Louis GERARD <186476141+lgerard-pass@users.noreply.github.com>
2026-07-08 16:24:43 +0200

    add shebang

--- super-print.py
+++ super-print.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python
 import sys
+
 
 def super_print(msg: str) -> None:
     print("SUUUUUUUUUPER PRIIIIIIIIIINT")
     print(msg)
 
+
 if __name__ == "__main__":
     super_print(sys.argv[1])
+
